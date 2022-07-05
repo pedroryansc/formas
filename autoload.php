62fbedb90d47098d0c712b2fddd60c886e60f467
Covid 19 <covid@IFRSL.redelocal>
2022-07-05 16:51:43 -0300

    Teste do "autoload"

--- autoload.php
+++ autoload.php
@@ -1,5 +1,5 @@
 <?php
     spl_autoload_register(function($class){
-        include "../class/".$class.".class.php";
+        include "class/".$class.".class.php";
     });
 ?>
---
+++ teste.php
@@ -0,0 +1,7 @@
+<?php
+    require_once("autoload.php");
+    $quad = new Quadrado(1, 1, 1, 1);
+    echo $quad;
+    $tab = new Tabuleiro(1, 1);
+    echo $tab;
+?>
